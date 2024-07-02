f7e0110fb6fbbcc5c5748e3cd7e02c776352027d
bigh0124 <thubgi0629@gmail.com>
2024-07-02 16:12:55 +0800

    feat: add logo image to navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Menu from "./Menu";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
@@ -13,7 +14,12 @@ const Navbar = () => {
         {/* BIGGER SCREEN */}
         <div className="hidden md:flex items-center justify-between gap-8 h-full">
           {/* LEFT */}
-          <div className="w-1/3"></div>
+          <div className="w-1/3">
+            <Link href="/">
+              <Image src="/logo.png" height={24} width={24} alt="Logo" />
+              <div className="text-2xl tracking-wide">E-Shop</div>
+            </Link>
+          </div>
           {/* RIGHT */}
           <div className="w-2/3"></div>
         </div>
