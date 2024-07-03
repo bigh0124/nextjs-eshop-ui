e44ca5741697a76763f8d101220eebe46333959d
bigh0124 <thubgi0629@gmail.com>
2024-07-03 18:43:28 +0800

    feat: add ProductList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Slider from "../components/Slider";
+import ProductList from "@/components/ProductList";
 
 const Home = () => {
   return (
     <div>
       <Slider />
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl text-center">Feature Product</h1>
+        <ProductList />
+      </div>
     </div>
   );
 };
