84ece6a5e7aae742cb952e29df588bbc571f5883
bigh0124 <thubgi0629@gmail.com>
2024-07-04 14:21:53 +0800

    feat: add ProductList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -27,6 +27,75 @@ const ProductList = () => {
           Add to cart
         </button>
       </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%] ">
+        <div className="relative w-full h-80">
+          <Image
+            src="/product.jpg"
+            layout="fill"
+            objectPosition="center"
+            alt=""
+            className="absolute object-cover z-20 hover:opacity-0 transition-all duration-500 ease-in"
+          />
+          <Image src="/product.png" layout="fill" objectPosition="center" alt="" className="absolute object-cover" />
+        </div>
+        <div className="flex items-center justify-between">
+          <h3 className="font-medium">Product name</h3>
+          <span className="font-semibold">$25</span>
+        </div>
+        <p className="text-sm text-gray-500">description of product</p>
+        <button
+          type="button"
+          className="rounded-2xl ring-1 ring-lama text-lama self-start py-2 px-4 text-xs hover:bg-rd hover:text-white"
+        >
+          Add to cart
+        </button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%] ">
+        <div className="relative w-full h-80">
+          <Image
+            src="/product.jpg"
+            layout="fill"
+            objectPosition="center"
+            alt=""
+            className="absolute object-cover z-20 hover:opacity-0 transition-all duration-500 ease-in"
+          />
+          <Image src="/product.png" layout="fill" objectPosition="center" alt="" className="absolute object-cover" />
+        </div>
+        <div className="flex items-center justify-between">
+          <h3 className="font-medium">Product name</h3>
+          <span className="font-semibold">$25</span>
+        </div>
+        <p className="text-sm text-gray-500">description of product</p>
+        <button
+          type="button"
+          className="rounded-2xl ring-1 ring-lama text-lama self-start py-2 px-4 text-xs hover:bg-rd hover:text-white"
+        >
+          Add to cart
+        </button>
+      </Link>
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%] ">
+        <div className="relative w-full h-80">
+          <Image
+            src="/product.jpg"
+            layout="fill"
+            objectPosition="center"
+            alt=""
+            className="absolute object-cover z-20 hover:opacity-0 transition-all duration-500 ease-in"
+          />
+          <Image src="/product.png" layout="fill" objectPosition="center" alt="" className="absolute object-cover" />
+        </div>
+        <div className="flex items-center justify-between">
+          <h3 className="font-medium">Product name</h3>
+          <span className="font-semibold">$25</span>
+        </div>
+        <p className="text-sm text-gray-500">description of product</p>
+        <button
+          type="button"
+          className="rounded-2xl ring-1 ring-lama text-lama self-start py-2 px-4 text-xs hover:bg-rd hover:text-white"
+        >
+          Add to cart
+        </button>
+      </Link>
     </div>
   );
 };
