9e7347e6e509d711068b98ba8733d5cafd44bd19
bigh0124 <thubgi0629@gmail.com>
2024-07-04 14:52:24 +0800

    feat: add CategoryList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Slider from "../components/Slider";
 import ProductList from "@/components/ProductList";
+import CategoryList from "@/components/CategoryList";
 
 const Home = () => {
   return (
@@ -9,6 +10,10 @@ const Home = () => {
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl text-center">Feature Product</h1>
         <ProductList />
+      </div>
+      <div className="mt-24">
+        <h1 className="text-2xl text-center">Categories</h1>
+        <CategoryList />
       </div>
     </div>
   );
