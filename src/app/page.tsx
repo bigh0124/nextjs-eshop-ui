2252776c09972f3a1df06521e728807faf88eb43
bigh0124 <thubgi0629@gmail.com>
2024-07-04 17:01:17 +0800

    modify: change div to section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <div>
       <Slider />
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <section className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl text-center">Feature Product</h1>
         <ProductList />
-      </div>
-      <div className="mt-24">
+      </section>
+      <section className="mt-24">
         <h1 className="text-2xl text-center">Categories</h1>
         <CategoryList />
-      </div>
+      </section>
     </div>
   );
 };
